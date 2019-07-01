03ade34c27c141f5891100d5ec6407909116dd6e
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-07-02 00:05:23 +0200

    first steps, SPA React scaffolder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const axios = require('axios');
+  axios.get('https://gateway.marvel.com:443/v1/public/events/29/characters?limit=58&apikey=10ac447eeaab932197ccab181c31c749')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
   return (
     <div className="App">
       <header className="App-header">
