ae82f8669b04001a9bf6e408b077b7001ecd740d
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-07-09 19:22:45 +0200

    Updates

---
+++ precache-manifest.446c6bdd66499e369b709f26fae413a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97dc0557f21abc540e52244c0d1aba61",
+    "url": "/marvel/index.html"
+  },
+  {
+    "revision": "05393ea5526a97cd2c2b",
+    "url": "/marvel/static/css/main.73f2aee1.chunk.css"
+  },
+  {
+    "revision": "2c678d0d133a149c77b3",
+    "url": "/marvel/static/js/2.ca35b799.chunk.js"
+  },
+  {
+    "revision": "05393ea5526a97cd2c2b",
+    "url": "/marvel/static/js/main.7103b234.chunk.js"
+  },
+  {
+    "revision": "03ccc9b3cbec2ecd8979",
+    "url": "/marvel/static/js/runtime~main.841ba7ac.js"
+  },
+  {
+    "revision": "067285b0c7e98187bfeb0fe492712d2e",
+    "url": "/marvel/static/media/Marvel-font.067285b0.ttf"
+  },
+  {
+    "revision": "da9deb97f85d8c29df86a53e4a485ea0",
+    "url": "/marvel/static/media/backgroundImg2.da9deb97.jpg"
+  },
+  {
+    "revision": "c514e34786695def10f57bd3d01e3567",
+    "url": "/marvel/static/media/backgroundImg4.c514e347.jpg"
+  },
+  {
+    "revision": "c3fcc390427d68b89647e834adf7fc87",
+    "url": "/marvel/static/media/linkedinLogo.c3fcc390.png"
+  }
+]);
